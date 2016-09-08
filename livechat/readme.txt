ca70ed4c24b58e8e7abf7cad429674ffdf46459a
JasonChi <jason79720@gmail.com>
2016-09-08 14:26:40 +0800

    LiveChat module added, with readme.txt

---
+++ livechat/readme.txt
@@ -0,0 +1,7 @@
+LiveChat library usage:
+
+1. new LiveChat(String username, String password, String registrationID, Context);
+
+2. public method: getChatUrl();
+
+3. WebView.loadUrl("ChatUrl") load up Live Chat Windows;
---
+++ livechat/src/main/java/com/example/jac/livechat/LiveChat.java
@@ -0,0 +1,107 @@
+package com.example.jac.livechat;
+
+import android.content.Context;
+import android.util.Base64;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by JasonChi on 2016/9/8.
+ */
+public class LiveChat {
+
+    private HashMap<String,String> param,paramI;
+    private String registerUrl = "https://api.puristit.com/register";
+    private String initializeUrl = "https://api.puristit.com/initialize";
+    private String chatUrl;
+    private String clientApiKey = "ydNHyu6SxFBdmzU2QNOvVnIOUKefuQy";
+    private String username;
+    private String password;
+    private String platform = "Android";
+    private String regid;
+    private String p_username;
+    private String p_password = "";
+    RequestQueue queue;
+
+    public LiveChat(String user_name, String user_pwd, String regid, Context context){
+        this.username = user_name;
+        this.password = user_pwd;
+        this.regid = regid;
+
+        queue = Volley.newRequestQueue(context);
+
+        param = new HashMap<>();
+        param.put("username", this.username);
+        param.put("password", this.password);
+        param.put("name", this.username);
+        param.put("platform", this.platform);
+
+        sendRequest(registerUrl, param);
+
+    }
+
+    public String getChatUrl(){
+        return chatUrl + "?platform=Android&registration_id=" + regid + "&roomlist=1";
+    }
+
+    private void sendRequest(String url, final HashMap<String,String> param) {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(param),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Response", response.toString());
+                        try {
+                            JSONObject object = new JSONObject(response.toString());
+                            JSONObject obj = object.getJSONObject("result");
+                            if(obj.has("chat_url")){
+                                chatUrl = obj.getString("chat_url");
+                            }else {
+                                p_username = obj.getString("p_username");
+                                p_password = obj.getString("p_password");
+
+                                paramI = new HashMap<>();
+                                paramI.put("p_username", p_username);
+                                paramI.put("p_password", p_password);
+                                paramI.put("name", username);
+                                paramI.put("platform", platform);
+                                paramI.put("registration_id", regid);
+                                sendRequest(initializeUrl, paramI);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("onErrorResponse: ", error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                String apiKey = clientApiKey +":"+ p_password;
+                String auth = "Basic " + Base64.encodeToString(apiKey.getBytes(), Base64.NO_WRAP);
+                HashMap<String,String> headers = new HashMap<>();
+                headers.put("Authorization",auth);
+
+                return  headers;
+            }
+        };
+        queue.add(jsonObjectRequest);
+    }
+
+}
